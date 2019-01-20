b9b2edb99ca4c255db4fdd7c27a7e7d0fe0fcaf2
varvarychA <av@marmala.com.ua>
2019-01-20 21:48:12 +0200

    All Html+CSS without fonts

--- app/js/common.js
+++ app/js/common.js
@@ -21,7 +21,31 @@ jQuery(document).ready(function() {
 /*----- Запуск слайдера галлереи ------*/
 $('.gallery').slick({
     infinite: true,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '0px',
     slidesToShow: 3,
-    slidesToScroll: 3
-  });
+    autoplay: true,
+    autoplaySpeed: 2410,
+    responsive: [
+        {
+        breakpoint: 768,
+        settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '50px',
+            slidesToShow: 3
+        }
+        },
+        {
+        breakpoint: 480,
+        settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+        }
+        }
+    ]
+    });
 /*-----    ------*/
